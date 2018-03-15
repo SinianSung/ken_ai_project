b8f06975a1dbdbf56073db8c70349e8fc77f3741
Gregor Lüdi <gregor.luedi@ken.ch>
2018-03-15 19:07:36 +0100

    beginning of perceptron

--- neuronalnetwork/perceptron.py
+++ neuronalnetwork/perceptron.py
@@ -1,5 +1,20 @@
+import random
+
+class Perceptron:
+
+    num_input_nodes =0
+
+    input_nodes = []
+    weights = []
+
+    learning_rate = 0.002
+
+    def __init__(self, num):
+        num_input_nodes = num
+        for i in range(num):
+            weights[i] random.random()
+        
+    def guess(self, inputs):
+        pass
 
 
-class perceptron:
-
-    
